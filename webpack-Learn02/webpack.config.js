d0c8b42c0d346f6ac0623aa2b1796191f8b3be37
YJH <m17854239497@outlook.com>
2018-03-15 17:05:20 +0800

    每次生成代码后，清理dist文件夹

--- webpack-Learn02/webpack.config.js
+++ webpack-Learn02/webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 // 下面这个插件会自动为我们生成一个index.html
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+// 下面这个插件会为我们在每次构建的时候清理dist文件夹
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -8,6 +10,7 @@ module.exports = {
     print: './src/print.js'
   },
   plugins: [
+    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Output Management'
     })
