0d43b4450870ab831da3929de42c73a74114ec1b
YJH <m17854239497@outlook.com>
2018-03-15 17:00:16 +0800

    自动生成index.html，并将打包好的xx.bundle.js引入

--- webpack-Learn02/webpack.config.js
+++ webpack-Learn02/webpack.config.js
@@ -1,10 +1,17 @@
 const path = require('path');
+// 下面这个插件会自动为我们生成一个index.html
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
     app: './src/index.js',
     print: './src/print.js'
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    })
+  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
